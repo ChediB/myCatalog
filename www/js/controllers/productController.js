76f44577624a39e361e669b4a928637123a046a3
Yahia Mgarrech <mgarrechyahia92@gmail.com>
2015-11-13 17:01:22 +0100

    Prodoct Detail UX/UI
    User Interaction , only client side , waiting for you to update
    serverside with php

--- www/js/controllers/productController.js
+++ www/js/controllers/productController.js
@@ -10,7 +10,7 @@ myapp.controller('productDetailsController',['$scope', '$state','$ionicLoading',
 	
 	$scope.products = [];
 	$scope.wichproduct = $state.params.aId;
-	
+	$scope.product = {};
 	productService.getAllProducts().success(function(data){
    		$scope.products=data;
    		console.log(data);
@@ -19,17 +19,67 @@ myapp.controller('productDetailsController',['$scope', '$state','$ionicLoading',
 };
 	function productDetailsController($scope, $state,$ionicLoading, productService){
 	
-	 $scope.show = $ionicLoading.show({
-          content: 'Getting current location...',
-          showBackdrop: false
-        });
+	$scope.show = $ionicLoading.show({
+	    content: 'Getting current location...',
+	    showBackdrop: false
+	});
 	
 	productService.getProductDetail($state.params.aId).success(function(data){
    		$scope.product=data;
-   		 console.log( data);
-   		   $ionicLoading.hide();
-   	}).error(function(){ console.log("get product error."); });
-	
+   		$ionicLoading.hide();
+   	}).error(function(){ 
+   		console.log("GET PRODUCT DETAIL : Switch to offline mode");
+   		$scope.product={
+   				"id":1,
+   				"name":"Product1",
+   				"price":21,
+   				"likes":[2,5,23,4],
+   				"dislikes":[1],
+   				"image":"img/ionic.png",
+   				"description":"Hi, this is a description of the product"
+   			};
+   		$ionicLoading.hide();
+   	});
+
+   	productService.getProductComments($state.params.aId).success(function(data){
+   		$scope.product.comments = data;
+   	}).error(function(){
+   		console.log("GET COMMENTS OF THE PRODUCT : Switch to offline mode");
+   		$scope.product.comments = [
+   			{
+   				"id":1,
+   				"owner":1,
+   				"image":"img/ionic.png",
+   				"timeStamp":20151113,
+   				"body":"this is a really good product"
+   			},
+   			{
+   				"id":2,
+   				"owner":3,
+   				"image":"img/ionic.png",
+   				"timeStamp":20151115,
+   				"body":"Yes ! I like it two"
+   			}
+   		];
+   	});
+
+   	$scope.Like = function(){
+   		productService.interact("add","like",$scope.product.id).success(function(data){
+   			$scope.product.likes = data;
+   		});
+   	};
+
+   	$scope.Dislike = function(){
+   		productService.interact("add","dislike",$scope.product.id).success(function(data){
+   			$scope.product.dislikes = data;
+   		});
+   	};
+
+	$scope.Comment = function(comment){
+   		productService.interact("add",comment,$scope.product.id).success(function(){
+   			$scope.product.comments = data;
+   		});
+   	};
 };
 
 	})();
--- www/js/services/productService.js
+++ www/js/services/productService.js
@@ -16,9 +16,61 @@ angular.module("myCatalogue").factory('productService', function($http){
        		url: "http://www.nao-secretary.com/mycatalogue/getProduct.php?idProduct="+id,
        		data: {}
      	};
- console.log( req.url);
       	return $http(req);
 	};
+
+	productFactory.getProductComments = function(id){
+   
+		var req = {
+       		method: 'GET',
+       		url: "http://www.nao-secretary.com/mycatalogue/getProductComments.php?idProduct="+id,
+       		data: {}
+     	};
+      	return $http(req);
+	};
+
+	//action : "add"/"remove"
+	productFactory.comment = function(action, comment){
+		var req = {
+       		method: 'POST',
+       		url: "http://www.nao-secretary.com/mycatalogue/comment.php",
+       		data: {
+       			"action":action,
+       			"comment":comment
+       		}
+     	};
+      	return $http(req);
+	}
+
+
+	//action : "add"/"remove"
+	//type : "like"/"dislike"
+	productFactory.interact = function(action, type, interaction){ 
+		var req = {
+       		method: 'POST',
+       		url: "http://www.nao-secretary.com/mycatalogue/interaction.php",
+       		data: {
+       			"action":action,
+       			"type":type,
+       			"interaction":interaction
+       		}
+   		};
+      	return $http(req);
+	}
+
+	//action : "add"/"remove"
+	productFactory.shoppingList = function(action, product){
+		var req = {
+       		method: 'POST',
+       		url: "http://www.nao-secretary.com/mycatalogue/shoppingList.php",
+       		data: {
+       			"action":action,
+       			"type":type,
+       			"interaction":interaction
+       		}
+   		};
+		return $http(req);
+	} 
 
 	return productFactory;
 });
